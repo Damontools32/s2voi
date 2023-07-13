6c819acc3748df3a2fec3fa10b7ea352c0e4283d
Damontools32 <123245734+Damontools32@users.noreply.github.com>
2023-07-14 01:51:52 +0330

    Update s2voi.py

--- s2voi.py
+++ s2voi.py
@@ -9,19 +9,24 @@ bot_token = 'YOUR_TELEGRAM_BOT_TOKEN'
 
 client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 
-def download_video(url: str) -> str:
+def download_video_and_thumbnail(url: str):
     ytdl_opts = {
         "format": "bestvideo[height<=360]+bestaudio/best[height<=360]",
         "outtmpl": "video.%(ext)s",
+        "writethumbnail": True,
     }
 
     with yt_dlp.YoutubeDL(ytdl_opts) as ydl:
         ydl.download([url])
 
+    video_path, thumb_path = None, None
     for file in os.listdir():
         if file.startswith("video."):
-            return file
-    return None
+            video_path = file
+        elif file.startswith("video_thumbnail."):
+            thumb_path = file
+
+    return video_path, thumb_path
 
 @client.on(events.NewMessage(pattern='/start'))
 async def start(event):
@@ -35,10 +40,17 @@ async def handle_url(event):
     url = event.text
     await event.reply("در حال دانلود ویدیو...")
 
-    video_path = download_video(url)
+    video_path, thumb_path = download_video_and_thumbnail(url)
     if video_path:
-        await client.send_file(event.chat_id, video_path, supports_streaming=True)
+        await client.send_file(
+            event.chat_id,
+            video_path,
+            thumb=thumb_path if thumb_path else None,
+            supports_streaming=True,
+        )
         os.remove(video_path)
+        if thumb_path:
+            os.remove(thumb_path)
     else:
         await event.reply("مشکلی در دانلود ویدیو پیش آمده است.")
 
