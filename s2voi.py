48a418e58624ee2bbd434df7a0f9d0820d7bb0cd
Damontools32 <123245734+Damontools32@users.noreply.github.com>
2023-07-13 23:42:23 +0330

    Update s2voi.py

--- s2voi.py
+++ s2voi.py
@@ -1,23 +1,26 @@
+import os
 import asyncio
+import logging
 import requests
 
 from telethon import TelegramClient, events
 
-# جایگزین کردن توکن‌های مربوط به ربات تلگرام و Wit.ai
-API_ID = "YOUR_API_ID"
+# جایگزین کردن اطلاعات مربوط به کلاینت تلگرام و Wit.ai
+API_ID = YOUR_API_ID
 API_HASH = "YOUR_API_HASH"
 BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
 WIT_AI_SERVER_ACCESS_TOKEN = "YOUR_WIT_AI_SERVER_ACCESS_TOKEN"
 
 
-async def voice_to_text(voice_file) -> str:
+async def voice_to_text(file_path):
     api_url = "https://api.wit.ai/speech"
     headers = {
         "Content-Type": "audio/ogg; codecs=opus",
         "Authorization": f"Bearer {WIT_AI_SERVER_ACCESS_TOKEN}",
     }
 
-    response = requests.post(api_url, headers=headers, data=voice_file)
+    with open(file_path, "rb") as file:
+        response = requests.post(api_url, headers=headers, data=file.read())
 
     if response.status_code == 200:
         result = response.json()
@@ -27,22 +30,25 @@ async def voice_to_text(voice_file) -> str:
 
 
 async def main():
-    client = TelegramClient("bot", API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+    logging.basicConfig(level=logging.INFO)
+
+    client = TelegramClient("voice_bot", API_ID, API_HASH)
+    await client.start(bot_token=BOT_TOKEN)
 
     @client.on(events.NewMessage(pattern="/start"))
-    async def start_command_handler(event):
-        await event.reply("سلام! لطفا ویس خود را بفرستید تا آن را به متن تبدیل کنم.")
+    async def start_handler(event):
+        await event.respond("سلام! لطفا ویس خود را بفرستید تا آن را به متن تبدیل کنم.")
 
-    @client.on(events.NewMessage(func=lambda e: e.voice))
-    async def voice_message_handler(event):
-        voice = event.voice
-        voice_file = await client.download_file(voice)
-
-        try:
-            text = await voice_to_text(voice_file)
-            await event.reply(f"متن شناسایی شده: {text}")
-        except Exception as e:
-            await event.reply(f"مشکلی رخ داد: {e}")
+    @client.on(events.NewMessage)
+    async def voice_handler(event):
+        if event.message.voice:
+            try:
+                file_path = await event.message.download_media()
+                text = await voice_to_text(file_path)
+                os.remove(file_path)
+                await event.reply(f"متن شناسایی شده: {text}")
+            except Exception as e:
+                await event.reply(f"مشکلی رخ داد: {e}")
 
     await client.run_until_disconnected()
 
