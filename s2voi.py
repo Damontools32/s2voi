3f1dbfbf4d880645070b80233e65d8076ad5ec12
Damontools32 <123245734+Damontools32@users.noreply.github.com>
2023-07-14 00:42:39 +0330

    Update s2voi.py

--- s2voi.py
+++ s2voi.py
@@ -2,22 +2,31 @@ import os
 import speech_recognition as sr
 from pyrogram import Client
 
-api_id = 12345 
+# نصب TgCrypto
+os.system("pip install tgcrypto")
+
+api_id = 12345  
 api_hash = 'your api hash'
-bot_token = 'bot token'
+bot_token = 'bot token' 
 
 app = Client("my_bot", api_id, api_hash, bot_token=bot_token)
 
 @app.on_message()
 async def get_voice(client, message):
+
   if message.voice:
-    voice_note = message.voice
-    voice_note.download("voice.ogg") 
-    os.system("ffmpeg -i voice.ogg voice.wav")
+  
+    # ذخیره voice note
+    voice_note = message.voice  
     
+    # دانلود voice note
+    voice_note.download("voice.ogg")
+
+    os.system("ffmpeg -i voice.ogg voice.wav")
+            
     r = sr.Recognizer()
     with sr.AudioFile('voice.wav') as source:
-      audio = r.record(source)  
+      audio = r.record(source)
 
     text = r.recognize_google(audio, language='fa-IR')
     
