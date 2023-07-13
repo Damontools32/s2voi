a989125ccdcd7c1e092ccc35123ce9608747e4f7
Damontools32 <123245734+Damontools32@users.noreply.github.com>
2023-07-14 00:36:52 +0330

    Update s2voi.py

--- s2voi.py
+++ s2voi.py
@@ -1,25 +1,26 @@
 import os
-import speech_recognition as sr 
+import speech_recognition as sr
 from pyrogram import Client
 
-api_id = 12345
+api_id = 12345 
 api_hash = 'your api hash'
-bot_token = '12345:YOUR_BOT_TOKEN' 
+bot_token = 'bot token'
 
-bot = Client("my_bot", api_id, api_hash, bot_token=bot_token)
+app = Client("my_bot", api_id, api_hash, bot_token=bot_token)
 
-# دریافت ویس از کاربر
-message = bot.ask("لطفا یک پیام صوتی بفرستید")  
-voice_note = message.voice
-voice_note.download("voice.ogg")
-os.system("ffmpeg -i voice.ogg voice.wav")
+@app.on_message()
+async def get_voice(client, message):
+  if message.voice:
+    voice_note = message.voice
+    voice_note.download("voice.ogg") 
+    os.system("ffmpeg -i voice.ogg voice.wav")
+    
+    r = sr.Recognizer()
+    with sr.AudioFile('voice.wav') as source:
+      audio = r.record(source)  
 
-# تبدیل ویس به متن   
-r = sr.Recognizer()
-with sr.AudioFile('voice.wav') as source:
-    audio = r.record(source)
+    text = r.recognize_google(audio, language='fa-IR')
+    
+    await app.send_message(message.chat.id, text)
 
-text = r.recognize_google(audio, language='fa-IR')
-
-# ارسال متن به کاربر
-bot.send_message(message.chat.id, text)
+app.run()
