02652e3a49cdc0bf05066900e1ea956b7bf1409d
Damontools32 <123245734+Damontools32@users.noreply.github.com>
2023-07-14 02:04:08 +0330

    Update s2voi.py

--- s2voi.py
+++ s2voi.py
@@ -2,8 +2,6 @@ import os
 import asyncio
 from telethon import TelegramClient, events
 import yt_dlp
-from moviepy.editor import *
-from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 
 api_id = YOUR_API_ID
 api_hash = 'YOUR_API_HASH'
@@ -11,32 +9,19 @@ bot_token = 'YOUR_TELEGRAM_BOT_TOKEN'
 
 client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 
-def download_video_and_thumbnail(url: str):
+def download_video(url: str) -> str:
     ytdl_opts = {
         "format": "bestvideo[height<=360]+bestaudio/best[height<=360]",
         "outtmpl": "video.%(ext)s",
-        "writethumbnail": True,
     }
 
     with yt_dlp.YoutubeDL(ytdl_opts) as ydl:
         ydl.download([url])
 
-    video_path, thumb_path = None, None
     for file in os.listdir():
         if file.startswith("video."):
-            video_path = file
-        elif file.startswith("video_thumbnail."):
-            thumb_path = file
-
-    return video_path, thumb_path
-
-def add_thumbnail_to_video(video_path, thumb_path):
-    video = VideoFileClip(video_path)
-    thumbnail = ImageClip(thumb_path, duration=video.duration)
-    final = CompositeVideoClip([thumbnail.set_opacity(0.6), video.set_opacity(0.4)])
-    final_path = "final_" + video_path
-    final.write_videofile(final_path, codec="libx264", audio_codec="aac")
-    return final_path
+            return file
+    return None
 
 @client.on(events.NewMessage(pattern='/start'))
 async def start(event):
@@ -50,17 +35,10 @@ async def handle_url(event):
     url = event.text
     await event.reply("در حال دانلود ویدیو...")
 
-    video_path, thumb_path = download_video_and_thumbnail(url)
-    if video_path and thumb_path:
-        final_video_path = add_thumbnail_to_video(video_path, thumb_path)
-        await client.send_file(
-            event.chat_id,
-            final_video_path,
-            supports_streaming=True,
-        )
+    video_path = download_video(url)
+    if video_path:
+        await client.send_file(event.chat_id, video_path, supports_streaming=True)
         os.remove(video_path)
-        os.remove(thumb_path)
-        os.remove(final_video_path)
     else:
         await event.reply("مشکلی در دانلود ویدیو پیش آمده است.")
 
