bacacb6a5b764ae573d82ba8b2ba03c399868190
Damontools32 <123245734+Damontools32@users.noreply.github.com>
2023-07-20 12:24:44 +0330

    Create s2voi2.py

---
+++ s2voi2.py
@@ -0,0 +1,64 @@
+import os
+import asyncio
+import aiohttp
+from pyrogram import Client, filters
+import yt_dlp
+
+api_id = YOUR_API_ID
+api_hash = 'YOUR_API_HASH'
+bot_token = 'YOUR_TELEGRAM_BOT_TOKEN'
+
+app = Client("bot", api_id, api_hash, bot_token=bot_token)
+
+def download_video(url: str) -> str:
+    ytdl_opts = {
+        "format": "bestvideo[height<=360]+bestaudio/best[height<=360]",
+        "outtmpl": "video.%(ext)s",
+    }
+
+    with yt_dlp.YoutubeDL(ytdl_opts) as ydl:
+        ydl.download([url])
+
+    for file in os.listdir():
+        if file.startswith("video."):
+            return file
+    return None
+
+async def upload_url(chat_id, url: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status == 200:
+                file_name = url.split("/")[-1]
+                with open(file_name, "wb") as file:
+                    while True:
+                        chunk = await response.content.read(1024)
+                        if not chunk:
+                            break
+                        file.write(chunk)
+                await app.send_message(chat_id, "در حال آپلود فایل...")
+                await app.send_document(chat_id, file_name)
+                os.remove(file_name)
+            else:
+                await app.send_message(chat_id, "مشکلی در دانلود فایل پیش آمده است.")
+
+@app.on_message(filters.command("start"))
+async def start(client, message):
+    await message.reply_text("لطفا لینک ویدیوی یوتیوب خود را بفرستید یا لینک مستقیم فایل را برای آپلود به تلگرام ارسال کنید.")
+
+@app.on_message(filters.text & ~filters.command)
+async def handle_url(client, message):
+    url = message.text
+
+    if "youtube.com" in url or "youtu.be" in url:
+        await message.reply_text("در حال دانلود ویدیو...")
+        video_path = download_video(url)
+        if video_path:
+            await client.send_document(message.chat.id, video_path)
+            os.remove(video_path)
+        else:
+            await message.reply_text("مشکلی در دانلود ویدیو پیش آمده است.")
+    else:
+        await message.reply_text("در حال دانلود فایل...")
+        await upload_url(message.chat.id, url)
+
+app.run()
