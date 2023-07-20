905da6d7d9a83d64bead59dac6bf519d8f3cede2
Damontools32 <123245734+Damontools32@users.noreply.github.com>
2023-07-20 16:12:06 +0330

    Update s2voi2.py

--- s2voi2.py
+++ s2voi2.py
@@ -1,5 +1,4 @@
 import os
-import aiohttp
 from pyrogram import Client, filters
 import yt_dlp
 
@@ -9,11 +8,10 @@ bot_token = 'YOUR_TELEGRAM_BOT_TOKEN'
 
 app = Client("bot", api_id, api_hash, bot_token=bot_token)
 
-def download_video_and_thumbnail(url: str):
+def download_video(url: str):
     ytdl_opts = {
         "format": "bestvideo[height<=360][ext=webm]+bestaudio[ext=webm]/best[height<=360][ext=webm]",
         "outtmpl": "video.%(ext)s",
-        "writethumbnail": True
     }
 
     with yt_dlp.YoutubeDL(ytdl_opts) as ydl:
@@ -23,14 +21,11 @@ def download_video_and_thumbnail(url: str):
             print(f'Error during download: {e}')
 
     video_file = None
-    thumbnail_file = None
     for file in os.listdir():
         if file.startswith("video."):
             video_file = file
-        elif file.endswith((".jpg", ".png")):
-            thumbnail_file = file
 
-    return video_file, thumbnail_file
+    return video_file
 
 @app.on_message(filters.command("start"))
 async def start(client, message):
@@ -46,14 +41,12 @@ async def handle_url(client, message):
 
     if "youtube.com" in url or "youtu.be" in url:
         await message.reply_text("در حال دانلود ویدیو...")
-        video_file, thumbnail_file = download_video_and_thumbnail(url)
-        if video_file and thumbnail_file:
-            await client.send_video(message.chat.id, video_file, thumb=thumbnail_file)
+        video_file = download_video(url)
+        if video_file:
+            await client.send_video(message.chat.id, video_file, supports_streaming=True)
             if os.path.exists(video_file):
                 os.remove(video_file)
-            if os.path.exists(thumbnail_file):
-                os.remove(thumbnail_file)
         else:
-            await message.reply_text("مشکلی در دانلود ویدیو یا تصویر شاخص پیش آمده است.")
+            await message.reply_text("مشکلی در دانلود ویدیو پیش آمده است.")
 
 app.run()
